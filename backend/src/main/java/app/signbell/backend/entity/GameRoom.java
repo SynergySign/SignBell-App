99fc8914be584ffe28d86f038a48388ce935c0d0
Kanggwanju <xprtktm1@gmail.com>
2025-10-13 16:24:45 +0900

    feat: GameRoom 엔티티의 생성자에 @Builder 추가 (#11)
    - GameRoom 엔티티에 @Builder 어노테이션 및 필드 초기화 생성자 추가
    - 주어진 게임 제목, 호스트, 상태 값을 기반으로 기본 설정값을 갖는 인스턴스 생성 가능
    - 초기 maxParticipants, currentParticipants, currentRound 값 설정

--- backend/src/main/java/app/signbell/backend/entity/GameRoom.java
+++ backend/src/main/java/app/signbell/backend/entity/GameRoom.java
@@ -2,6 +2,7 @@ package app.signbell.backend.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -70,6 +71,16 @@ public class GameRoom {
      */
     @UpdateTimestamp
     private LocalDateTime updatedAt;
+
+    @Builder
+    public GameRoom(String gameTitle, User host, GameRoomStatus status) {
+        this.gameTitle = gameTitle;
+        this.host = host;
+        this.status = status;
+        this.maxParticipants = 4;
+        this.currentParticipants = 1;
+        this.currentRound = 1;
+    }
 
     // --- 편의 메서드 ---
 
