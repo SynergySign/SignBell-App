f78b12b112f22a0c1daf61293ba66e2fdf6616a6
sdj3959 <dj9099@naver.com>
2025-10-17 18:11:18 +0900

    feat: [MAIN-001] 메인 페이지 라우팅 및 기본 레이아웃 구현
    - /main 경로 추가 및 MainPage 컴포넌트 생성
    - 3단 구조 (Header, Main, Footer) 레이아웃 구성
    - 페이지 진입 애니메이션 (fadeIn) 적용
    - Primary Color 배경 적용

---
+++ frontend/src/pages/main/MainPage.jsx
@@ -0,0 +1,22 @@
+/**
+ * @개요 메인 페이지 컴포넌트
+ * @작성자 신동준 (sdj3959)
+ * @작성일 2025-10-17
+ * @최종수정일 2025-10-17
+ * @반환값 {JSX.Element} 메인 페이지 컴포넌트
+ */
+
+import './MainPage.scss';
+
+const MainPage = () => {
+  return (
+    <div className="main-page">
+      <div className="main-content">
+        <h1>메인 페이지</h1>
+        {/* TODO: 프로필 카드 및 기능 버튼 구현 예정 */}
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
