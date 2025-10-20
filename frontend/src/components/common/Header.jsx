08e803307043f889e2a63b8a267d3673192f3409
sdj3959 <dj9099@naver.com>
2025-10-20 09:38:35 +0900

    feat: [MAIN-001] 헤더에 마이페이지 및 로그아웃 버튼 추가
    - 헤더에 마이페이지 버튼 추가 (아이콘 + 텍스트)
    - 로그아웃 버튼 기능 구현
    - 메인 페이지에서만 버튼 표시 (useLocation 활용)
    - 모바일에서는 아이콘만 표시
    - 버튼 호버 효과 및 스타일링 적용

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -2,19 +2,41 @@
  * @개요 공통 헤더 컴포넌트
  * @작성자 신동준 (sdj3959)
  * @작성일 2025-10-17
- * @최종수정일 2025-10-17
+ * @최종수정일 2025-10-20
  * @반환값 {JSX.Element} 헤더 컴포넌트
  */
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import './Header.scss';
 
 const Header = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleLogoClick = () => {
+    // 메인 페이지에서는 새로고침, 다른 페이지에서는 메인으로 이동
+    if (location.pathname === '/main') {
+      window.location.reload();
+    } else {
+      navigate('/');
+    }
+  };
+
+  const handleMyPageClick = () => {
+    // TODO: 마이페이지 구현 후 연결
+    console.log('마이페이지로 이동');
+  };
+
+  const handleLogoutClick = () => {
+    // TODO: 로그아웃 API 연동 필요
+    console.log('로그아웃');
     navigate('/');
   };
+
+  // 메인 페이지에서만 마이페이지와 로그아웃 버튼 표시
+  const showMainPageButtons = location.pathname === '/main';
 
   return (
     <header className="header">
@@ -23,7 +45,26 @@ const Header = () => {
           SignBell
         </div>
         <div className="header-actions">
-          {/* TODO: 로그인 상태에 따라 버튼 표시 변경 */}
+          {showMainPageButtons && (
+            <>
+              <button
+                className="header-button mypage-button"
+                onClick={handleMyPageClick}
+                aria-label="마이페이지"
+              >
+                <FontAwesomeIcon icon={faUser} />
+                <span>마이페이지</span>
+              </button>
+              <button
+                className="header-button logout-button"
+                onClick={handleLogoutClick}
+                aria-label="로그아웃"
+              >
+                <FontAwesomeIcon icon={faRightFromBracket} />
+                <span>로그아웃</span>
+              </button>
+            </>
+          )}
         </div>
       </div>
     </header>
