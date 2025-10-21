b538a3db029bf88a542c5060947ef5d3edbb7689
songkey06 <mjsong0524@gmail.com>
2025-10-21 15:40:12 +0900

    chore: `vite-plugin-mkcert` 개발 의존성 추가 (#67)
    - 개발 환경에서 HTTPS 지원을 위한 `vite-plugin-mkcert` 패키지 추가
    - `package.json` 및 `package-lock.json` 파일 업데이트
    - 로컬 HTTPS 설정 간소화로 개발/테스트 환경 일관성 강화

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      mkcert()
+  ],
 
   // CSS 설정
   css: {
@@ -15,11 +19,12 @@ export default defineConfig({
 
   // 개발 서버 설정을 추가합니다.
   server: {
+    https: true,
     proxy: {
       // '/api'로 시작하는 모든 요청을 target 주소로 전달(프록시)합니다.
       '/api': {
         // target: 'http://127.0.0.1:8000 ', // FastAPI 서버의 주소
-        target: 'http://localhost:9000',
+        target: 'https://localhost:8443',
         changeOrigin: true, // CORS 오류를 방지하기 위해 origin 헤더를 변경합니다.
         ws: true, // WebSocket 프록시를 활성화합니다. (매우 중요!)
         // 경로를 다시 작성합니다. 예: /api/ws -> /ws
